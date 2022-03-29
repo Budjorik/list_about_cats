5b10393e49e1b675926ab32a72bee062c9a27773
Сергей Караульных <karaulnih@gmail.com>
2022-03-29 15:47:27 +0500

    Create files

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/" +
+            "netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper(); // создаем 'json mapper'
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI); // содаем объект запроса
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = response = httpClient.execute(request); // создаем запрос
+
+        List<Post> posts = mapper.readValue(response.getEntity().getContent(),
+                new TypeReference<List<Post>>() {}); // преобразовываем 'json' в 'java'
+        posts.stream() // создаем поток
+                .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0) // фильтруем
+                .forEach(System.out::println); // выаодим в консоль
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,54 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post {
+    private final String id; // уникальный идентификатор записи
+    private final String text; // сообщение
+    private final String type; // тип животного
+    private final String user; // имя пользователя
+    private final Integer upvotes; // голоса
+
+    public Post( // конструктор класса
+        @JsonProperty("id") String id,
+        @JsonProperty("text") String text,
+        @JsonProperty("type") String type,
+        @JsonProperty("user") String user,
+        @JsonProperty("upvotes") Integer upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override // переопределяем метод 'toString'
+    public String toString() {
+        return "Posr" +
+            "\n id=" + id +
+            "\n text=" + text +
+            "\n type=" + type +
+            "\n user=" + user +
+            "\n upvotes=" + upvotes;
+    }
+
+}
